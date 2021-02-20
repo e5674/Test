b7a9e6ea20e3c39de612c96ef85cf4702acf0d1e
e5674 <79358393+e5674@users.noreply.github.com>
2021-02-20 13:19:21 +0300

    1234

---
+++ bullet.py
@@ -0,0 +1,5 @@
+import pygame
+from pygame.sprit import sprite
+
+class Bullet(sprite)#Класс для управления снарядами
+    def __
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings(): # класс для хранения настроек игры
+    def __init__(self):
+        #параметры экрана
+        self.screen_width = 1200
+        self.screen_heigth = 700
+        self.bg_color = (0, 59, 89)
+
